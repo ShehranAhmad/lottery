d647e63b29a2ff9e59df512587a46c9a69651281
xhery_saith12 <shehran.ahmad@leadpops.com>
2022-10-19 14:31:27 +0500

    update sitemap

--- resources/views/frontend/sitemap.blade.php
+++ resources/views/frontend/sitemap.blade.php
@@ -1,38 +1,22 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 
-<urlset
-    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-    <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
-
-
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
     <url>
         <loc>https://lotterybazarfast.in/</loc>
-        <lastmod>2022-10-06T13:15:03+00:00</lastmod>
-        <priority>1.00</priority>
+    </url>
+    <url>
+        <loc>https://lotterybazarfast.in/blogDetail/lottery-bazar-result</loc>
+    </url>
+    <url>
+        <loc>https://lotterybazarfast.in/blogDetail/raja-rani-coupon-result</loc>
     </url>
     <url>
         <loc>https://lotterybazarfast.in/blogs</loc>
-        <lastmod>2022-10-06T13:15:03+00:00</lastmod>
-        <priority>0.80</priority>
-    </url>
-    <url>
-        <loc>https://lotterybazarfast.in/results</loc>
-        <lastmod>2022-10-06T13:15:03+00:00</lastmod>
-        <priority>0.80</priority>
     </url>
     <url>
         <loc>https://lotterybazarfast.in/contact</loc>
-        <lastmod>2022-10-06T13:15:03+00:00</lastmod>
-        <priority>0.80</priority>
     </url>
     <url>
-        <loc>https://lotterybazarfast.in/blogDetail/how-to-get-lottery-bazar-result-in-various-regions-of-india</loc>
-        <lastmod>2022-10-06T13:15:03+00:00</lastmod>
-        <priority>0.64</priority>
+        <loc>https://lotterybazarfast.in/results</loc>
     </url>
-
-
 </urlset>

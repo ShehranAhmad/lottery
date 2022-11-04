55d1171b15ac2fa34ec562f3a3d7a80b3d83e0e4
Shehran Ahmad <shehran.ahmad@leadpops.com>
2022-11-04 16:04:02 +0500

    remove extra slash

--- resources/views/frontend/sitemap.blade.php
+++ resources/views/frontend/sitemap.blade.php
@@ -2,7 +2,7 @@
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
     <url>
-        <loc>https://lotterybazarfast.in/</loc>
+        <loc>https://lotterybazarfast.in</loc>
     </url>
     <url>
         <loc>https://lotterybazarfast.in/blogDetail/lottery-bazar-result</loc>
